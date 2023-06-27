d058596838afb77dbfc7bfe80327a7c3d389e2e2
Bhuvnesh <109529957+bhuvn3sh@users.noreply.github.com>
2023-06-27 22:39:00 +0530

    Delete src directory

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Navbar } from './Navbar';
-export { default as Header } from './Header';
-export { default as Product } from './Product';
-export { default as Products } from './Products';
-export { default as Footer } from './Footer';
